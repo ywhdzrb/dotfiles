ac3c0ce9e89ff38915943def19a741af95329c32
ywhdzrb <intmainreturn@outlook.com>
2026-07-18 00:03:13 +0800

    add Japanese OCR support

--- .config/waybar/scripts/ocr-translate.sh
+++ .config/waybar/scripts/ocr-translate.sh
@@ -2,7 +2,7 @@
 TMPFILE=$(mktemp /tmp/ocr-XXXXXX.png)
 grim -g "$(slurp -d)" "$TMPFILE"
 convert "$TMPFILE" -resize 200% -unsharp 0x1 "$TMPFILE"
-TEXT=$(tesseract "$TMPFILE" - -l chi_sim+eng --oem 1 --psm 6 2>/dev/null | tr -s '\n' ' ')
+TEXT=$(tesseract "$TMPFILE" - -l chi_sim+eng+jpn --oem 1 --psm 6 2>/dev/null | tr -s '\n' ' ')
 rm "$TMPFILE"
 if [ -z "$TEXT" ]; then
   dunstify "📖 OCR 翻译" "未识别到文字"
--- .config/waybar/scripts/ocr.sh
+++ .config/waybar/scripts/ocr.sh
@@ -3,7 +3,7 @@ TMPFILE=$(mktemp /tmp/ocr-XXXXXX.png)
 grim -g "$(slurp -d)" "$TMPFILE"
 # 提高 OCR 精度：放大图片 + LSTM引擎 + 单文本块模式
 convert "$TMPFILE" -resize 200% -unsharp 0x1 "$TMPFILE"
-TEXT=$(tesseract "$TMPFILE" - -l chi_sim+eng --oem 1 --psm 6 2>/dev/null | tr -s '\n' ' ')
+TEXT=$(tesseract "$TMPFILE" - -l chi_sim+eng+jpn --oem 1 --psm 6 2>/dev/null | tr -s '\n' ' ')
 rm "$TMPFILE"
 if [ -n "$TEXT" ]; then
   echo "$TEXT" | wl-copy
